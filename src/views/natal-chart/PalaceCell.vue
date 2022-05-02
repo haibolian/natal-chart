c14fe874d32541a0b755af86a298c12bbe409b29
lianhaibo <957377783@qq.com>
2022-05-02 18:01:10 +0800

    宫样式

--- src/views/natal-chart/PalaceCell.vue
+++ src/views/natal-chart/PalaceCell.vue
@@ -3,7 +3,33 @@
     :class="`natal-chart__palace natal-chart__palace__${ dizhiCode }`"
     :style="{ gridArea: dizhiCode }"
   >
-    {{ tiangan }}
+    <div class="palace-stars">
+      <div class="palace-star palace-star__main" v-for="star in mainStars" :key="star.code">
+        <span>{{ star.name }}</span>
+        <span class="star-sihua">{{ star.sihua }}</span>
+      </div>
+
+      <div class="palace-star palace-star__sub" v-for="star in subStars" :key="star.code">
+        <span>{{ star.name }}</span>
+        <span class="star-sihua">{{ star.sihua }}</span>
+      </div>
+      <div class="palace-star palace-star__small" v-for="(star, index) in smallStars" :key="star.code">
+        <span>{{ star.name }}</span>
+      </div>
+    </div>
+
+    <div class="palace-info">
+      <div class="info palace-name">
+        {{ name }}
+      </div>
+      <div class="info flow-years">
+
+      </div>
+      <div class="info tiangan-dizhi">
+        {{ tiangan + dizhi }}
+      </div>
+    </div>
+    <!-- {{ tiangan }}
     {{ dizhi }}
     {{ isFate ? '命宫' : '' }}
     {{ isBody ? '身宫' : '' }}
@@ -11,9 +37,12 @@
     <span v-for="(star, index) in mainStars" :key="index">
      {{ star.name }}
     </span>
-    <span v-for="(star, index) in smallStars" :key="index">
-      {{ star.name }}
+    <span v-for="(star, index) in subStars" :key="star">
+    {{ star.name }}
     </span>
+    <span v-for="(star, index) in smallStars" :key="star">
+      {{ star.name }}
+    </span> -->
   </div>
 </template>
 
@@ -33,14 +62,63 @@ const props = defineProps({
   isBody: Boolean,
   name: String,
   mainStars: Array,
+  subStars: Array,
   smallStars: Array
 })
 </script>
 
 <style>
   .natal-chart__palace {
+    font-size: 1rem;
     background-color: #fff;
     box-sizing: border-box;
     border: 1px solid #00000080;
+  }
+  .palace-stars {
+    height: 40%;
+    display: flex;
+  }
+  .star-sihua {
+    color: #fff;
+    margin-top: .3rem;
+    background-color: rgb(255, 29, 67);
+    border-radius: 4px;
+  }
+  .palace-star {
+    line-height: 1.3;
+    writing-mode: vertical-lr;
+  }
+  .palace-star__main {
+    color: rgb(193, 83, 5);
+  }
+  .palace-star__sub {
+    color:rgb(200, 88, 200);
+  }
+  .palace-star__small {
+    color: rgb(67, 127, 239);
+  }
+
+  /* 宫信息 */
+  .palace-info {
+    height: 60%;
+    position: relative;
+  }
+  .info {
+    text-align: center;
+    position: absolute;
+  }
+  .palace-name {
+    width: 2rem;
+    background-color:rgb(0, 133, 205);
+    border-radius: 4px;
+    color: #fff;
+    bottom: 0;
+    left: 50%;
+    transform: translateX(-50%);
+  }
+  .tiangan-dizhi {
+    bottom: 0;
+    right: 0;
+    font-weight: bold;
   }
 </style>
